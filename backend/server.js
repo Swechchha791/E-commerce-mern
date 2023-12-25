bfc280dfcc960a0335729c7fcded98229f82b6d8
Swechchha <swechchhapandey271@gmail.com>
2023-12-25 16:39:16 +0530

    Custom Error handling

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,23 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 
+// Error for not Found Product
+app.use((req, res) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+// Error handling for Wrong Request Id
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+});
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(
